efd399fc849213e43a69f0a72b87d7d8c5d08230
Osvaldo Fernandez <osvaldo.fernandez@wolox.com.ar>
2015-08-06 13:04:54 -0300

    getting response

--- src/app/components/component2/component2Controller.js
+++ src/app/components/component2/component2Controller.js
@@ -4,7 +4,7 @@ function() {
 }]);
 
 
-angular.module('app').controller('UserController', [
+angular.module('app').controller('UserController', ['Restangular',
 function(Restangular) {
 	this.baseUsers = Restangular.all('users');
 	this.user={};
@@ -18,7 +18,12 @@ function(Restangular) {
 			"firstName": this.user.name,
 			"lastName": this.user.lastname
 		}
-		baseUsers.post(newUser);
+        this.baseUsers.post(newUser).then(function(resp) {
+            console.log("new Object id", resp.objectId);
+            console.log("session token", resp.sessionToken);
+        }, function(resp) {
+            console.log(resp.data.error);
+        });
 		this.user={};
 	}
 }]);
